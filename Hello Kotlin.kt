14e7f99356917de4a15bb801d2ed82fdceeebce1
FahmiAjiWicaksono <68975508+FahmiAjiWicaksono@users.noreply.github.com>
2020-08-07 19:48:42 +0700

    Add files via upload

---
+++ Hello Kotlin.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val nama = "Fahmi Aji Wicaksono"
+    val hobi = "Bermain Sepak Bola"
+
+    println ("Nama saya " + nama);
+    println ("Hobi saya " + hobi);
+}
---
+++ if expression.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val nilai: Int = 82
+    if (nilai >= 75) {
+        println("Nilai kamu $nilai, Selamat ya!")
+    } else {
+        println("Nilai kamu $nilai, Silahkan ikut remidial.")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
